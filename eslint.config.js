39b39d7f8fe79c7e335e26b014f33749ba302789
Onion <congma@outlook.com>
2025-01-01 15:06:13 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+// eslint.config.js
+import markdown from "@eslint/markdown";
+
+export default [
+    {
+        files: ["**/*.md"],
+        plugins: {
+            markdown
+        },
+        language: "markdown/commonmark",
+        rules: {
+            "markdown/no-html": "off",
+        }
+    }
+];
